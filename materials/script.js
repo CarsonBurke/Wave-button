9092e3f4c3f0bc499d4a482da5e87f903fac723b
Carson Burke <carsonburke22@gmail.com>
2021-07-21 18:32:46 -0700

    initial commit

---
+++ materials/script.js
@@ -0,0 +1,16 @@
+let waveButtons = document.getElementsByClassName("waveButton")
+
+window.onload = function() {
+
+    for (let button of waveButtons) {
+
+        button.style.position = relative
+
+        document.button.addEventListener(onmousedown, addWave)
+    }
+}
+
+function addWave() {
+
+
+}
